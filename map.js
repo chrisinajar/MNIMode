4fcc45550caf12ed73e8dbda48f51b3e3923bd9c
chrisinajar <chrisinajar@gmail.com>
2013-09-06 08:24:46 -0400

    Update map.js

--- map.js
+++ map.js
@@ -11,18 +11,8 @@ var configMap = {
 		"Entire Map": 0
 	},
 	MapBorders = {
-		rad: {
-			x: -3500,
-			y: -3100
-		},
-		dire: {
-			x: 3000,
-			y: 3000
-		},
-		lineR: 700,
-		lineD: 800,
-
-		secretShop: [936, -4197]
+		secretShop: [936, -4197],
+		runeSpawn: [1400, -4500]
 	};
 
 // set up game boundaries
@@ -33,12 +23,17 @@ game.hook("OnMapStart", onMapStart);
 function onMapStart() {
 	var wells, i, towers;
 	
+	
+	for(i = 0; i < server.clients.length; ++i) {
+		client = server.clients[i];
+		if (client && client.isInGame()) {
+			client.printToChat("Welcome to MNI Mode! This is an arena game mode where, after leaving the well, you will be teleported into a small battle area. Fight to the death, try to hold the center valley, and remember that you can only buy consumables with starting gold!");
+		}
+	}
+	
 	// get all the wells and remove them
 	// this keeps the heroes from healing themselves
-	wells = game.findEntitiesByClassname('ent_dota_fountain')
-	for (i=0; i<wells.length; i+= 1) {
-		dota.remove(wells[i]);
-	}
+	dota.removeAll("ent_dota_fountain");
 	
 	// taken directly from poor man's pudge wars
 	// Move T1 towers out of the way
@@ -69,6 +64,12 @@ function onMapStart() {
 	towers = game.findEntityByTargetname("dota_goodguys_tower3_bot");
 	if (towers != null) {
 		towers.teleport(-5000, -5000, 128);
+	}
+	
+	// rune spawners
+	wells = game.findEntitiesByClassname('dota_item_rune_spawner')
+	for (i=0; i<wells.length; i+= 1) {
+		wells[i].teleport(MapBorders.runeSpawn[0], MapBorders.runeSpawn[1], 256);
 	}
 
 }
